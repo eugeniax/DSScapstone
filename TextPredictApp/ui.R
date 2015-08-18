838719843b1cee3b14b346501686fa57d5f72457
eugeniax <eugenia.xu@gmail.com>
2015-08-18 16:55:24 +0800

    change folder name

--- TextPredictApp/ui.R
+++ TextPredictApp/ui.R
@@ -9,27 +9,27 @@ shinyUI(fluidPage(
                     Specialization") ),    
     fluidRow(
         br(),
-        p("This Shiny app takes an input from users and uses a simple ngram back-off model to predict next word. 
-          HC Corpora is pre-processed to build the frequency dictionary that is then loaded to Shiny app at runtime.
+        p("This Shiny app takes an input from users and uses a simple ngram back-off model to predict next word. ",
+          a("HC Corpora", href = "http://www.corpora.heliohost.org")," is pre-processed to build the frequency dictionary that is then loaded to Shiny app at runtime.
           The Shiny app will provide a few suggested words, if available, besides the predicted next word to ease user experience.")),
     br(),
   
-    fluidRow(strong("Enter at least 2 words. Press \"Next Word\" button to predict the next word.") ),
+    fluidRow(strong("Enter your text below. Press \"Next Word\" button to predict the next word.") ),
     br(),
     
     # Sidebar layout
     sidebarLayout(        
         sidebarPanel(
-            textInput("inText", "Enter your text here",value = "input text..."),
+            textInput("inText", "Enter your text",value = "e.g. Happy birthday to"),
             submitButton("Next Word")
         ),
         
         mainPanel(
-            h3("Predicted Next Word:"),
-            textOutput('predWd'),
+            h4("Predicted Next Word:"),
+            span(style="color:blue; font-weight:bold",(textOutput('predWd'))),
             h4("Other suggested words:"),
             textOutput('sugWd'),
-            h3("You have entered:"),
+            h4("You have entered:"),
             textOutput('repInText')
         )
     )
